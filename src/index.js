f74038a6a94e08ac7c3e1a77b08a8fdfc9a3a380
SMIT N DOSHI <smitdoshi007@gmail.com>
2016-11-02 15:46:20 -0400

    Experimental

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+
+import VideoList from './components/video_list';
 
 // Create a new component. This component should produce
 // Some HTML.
 
 const API_KEY = 'AIzaSyDiQ8-t0TzuHiqEmOzTiJMQXCUV_boOdaY';
 
-YTSearch({key:API_KEY, term:'surfborads'},function(data){
-  console.log(data);
-});
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state= { videos:[]};
+
+    YTSearch({key:API_KEY, term:'surfborads'}, (videos) => {
+      this.setState({videos});
+      // In ES6 this will resolve as this.setState({videos:videos});
+      // Only works when Key and Properties Name are same.
+
+    });
+  }
 
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos = {this.state.videos} />
+        
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it
